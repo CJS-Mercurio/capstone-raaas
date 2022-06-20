8742cea7b967e93b57d03817052bc3ce3acff515
aaron-cunanan <aaronyuu96@gmail.com>
2022-06-20 19:10:44 +0800

    updated
    up

--- modules/ResearchManagement/Views/Uploading/UserResearchV.php
+++ modules/ResearchManagement/Views/Uploading/UserResearchV.php
@@ -95,6 +95,13 @@
       <div class="col-lg-12 form-col">
 
                 <h3><?php echo $r['title']; ?></h3>
+                <!-- Working here Copyright-->
+                <?php if($r['privacy'] == 1): ?>
+                                          <?php if($r['file'] != ''): ?>
+                                            <p class="alert alert-info" style="width: 225px;"><b>This research is Copyrighted.</b></p>
+                                          <?php endif; ?>
+                                      <?php endif; ?>
+                   <!-- Working here Copyright-->
                 <div class="container research-body">
                   <div class="col-lg-12 research-col">
                     <div class="row">
